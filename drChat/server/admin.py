479b4c60de8deaf0d5ece9fa7ac2659ffb56c7e5
YashKharcheHS <yash.kharche@thehotspring.com>
2024-05-19 13:02:42 +0530

    setup tables

--- drChat/server/admin.py
+++ drChat/server/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import (
+    Category,
+    Server,
+    Channel
+)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+
+class ServerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'owner', 'category']
+
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'topic', 'owner', 'server']
+    
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Server, ServerAdmin)
+admin.site.register(Channel, ChannelAdmin)
