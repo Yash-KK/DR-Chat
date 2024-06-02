1ff21e5c1d84c3fd5952eb692d53a828e3503f9d
Yash-KK <reachyashk@gmail.com>
2024-06-02 08:46:49 +0530

    added PrimaryAppBar with ThemeProvider

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import Box from "@mui/material/Box";
+import PrimaryAppBar from "../components/PrimaryAppBar";
 
 const Home = () => {
   return (
-    <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
-      This Box renders as an HTML section element on Home.
+    <Box>      
+      <PrimaryAppBar />
     </Box>
   );
 };
