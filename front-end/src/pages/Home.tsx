b4bf6173f3c2aea36a1af7b0700983e1939d47b6
Yash-KK <reachyashk@gmail.com>
2024-06-02 14:02:21 +0530

    installed mui

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,6 +1,12 @@
-const Home = ()=>{
-    return (
-        <span>Home</span>
-    )
-}
+import Box from "@mui/material/Box";
+import CssBaseline from '@mui/material/CssBaseline';
+
+const Home = () => {
+  return (
+    <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
+        <CssBaseline />
+      This Box renders as an HTML section element.
+    </Box>
+  );
+};
 export default Home;
