7b35745af68192e06d36714ba06c8433d5e2e0ed
Yash-KK <reachyashk@gmail.com>
2024-08-07 22:18:35 +0530

    message history

--- drChat/server/urls.py
+++ drChat/server/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from .views import testing, ServerViewSet, CategoryViewSet
+from webchat.views import MessageViewSet
 
 router = DefaultRouter()
 router.register("api/server/select", ServerViewSet)
 router.register("api/server/category", CategoryViewSet)
+router.register("api/messages", MessageViewSet, basename="message")
 
 urlpatterns = [path("testing/", testing, name="testing")]
 urlpatterns = router.urls
