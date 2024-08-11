048a8e5ff5de5c8da93cdd8d406848df3ba925f7
Yash-KK <reachyashk@gmail.com>
2024-08-11 18:54:43 +0530

    endpoints

--- front-end/src/hooks/AuthServices.ts
+++ front-end/src/hooks/AuthServices.ts
@@ -2,6 +2,39 @@ import axios from "axios";
 import { AuthServiceProps } from "../@types/auth-serice";
 
 export function useAuthService(): AuthServiceProps {
+
+    const getUserIdFromToken = (access: string) => {
+        const token = access
+        const tokenParts = token.split(".")
+        const encodedPayLoad = tokenParts[1];
+        const decodedPayLoad = atob(encodedPayLoad);
+        const payLoadData = JSON.parse(decodedPayLoad);
+        const userId = payLoadData.user_id
+        return userId
+    }
+
+    const getUserDetails = async () => {
+        try {
+            const userId = localStorage.getItem("userId");
+            const accessToken = localStorage.getItem("access_token");
+
+            const response = await axios.get(
+                `http://127.0.0.1:8000/api/account/?user_id=${userId}`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+
+            }
+            )
+
+            const userDetails = response.data;
+            localStorage.setItem("username", userDetails.username);
+
+        } catch (err: any) {
+            return err
+        }
+    }
+
     const login = async (username: string, password: string) => {
         try {
             const response = await axios.post(
@@ -10,7 +43,15 @@ export function useAuthService(): AuthServiceProps {
                 password
             }
             )
-            console.log(response)
+            const { access, refresh } = response.data;
+
+            console.log(response.data)
+
+            localStorage.setItem("access_token", access);
+            localStorage.setItem("refresh_token", refresh);
+            localStorage.setItem("userId", getUserIdFromToken(access))
+
+            getUserDetails();
         } catch (err: any) {
             return err
         }
