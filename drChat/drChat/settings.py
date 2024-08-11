585ead28e5380a630b9df9b5f0d206afcc6dc930
Yash-KK <reachyashk@gmail.com>
2024-08-11 17:06:40 +0530

    jwt install

--- drChat/drChat/settings.py
+++ drChat/drChat/settings.py
@@ -137,7 +137,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+
     ]
 }
 
